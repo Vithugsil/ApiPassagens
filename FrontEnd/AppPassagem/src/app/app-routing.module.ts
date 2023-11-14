dedbd63217adbfefd83cbc84c135d0b55d2a59cb
vithugsil <vitorhugosilva012@gmail.com>
2023-11-14 10:26:38 -0300

    Adicionado components Aeroportos e Companhias

--- FrontEnd/AppPassagem/src/app/app-routing.module.ts
+++ FrontEnd/AppPassagem/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AeroportosComponent } from './components/aeroportos/aeroportos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'Aeroportos', component:AeroportosComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/AppPassagem/src/app/components/aeroportos/aeroportos.component.ts
+++ FrontEnd/AppPassagem/src/app/components/aeroportos/aeroportos.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AeroportosService } from 'src/app/Services/aeroportos.service'; 
+import { Aeroporto } from 'src/app/Classes/Aeroporto';
 
 @Component({
-  selector: 'app-aeroportos',
+  selector: 'app-aeroporto',
   templateUrl: './aeroportos.component.html',
   styleUrls: ['./aeroportos.component.css']
 })
-export class AeroportosComponent {
+export class AeroportosComponent implements OnInit {
+  formulario: any;
+  tituloFormulario: string = '';
+  
+  constructor(private aeroportosService : AeroportosService) { }
 
+  ngOnInit(): void {
+    this.tituloFormulario = 'Novo Aeroporto';
+    this.formulario = new FormGroup({
+      id: new FormControl(null),
+      nome: new FormControl(null),
+      cidade: new FormControl(null)
+    })
+  }
+  enviarFormulario(): void {
+    const aeroporto : Aeroporto = this.formulario.value;
+    this.aeroportosService.cadastrar(aeroporto).subscribe(result => {
+      alert('Carro inserido com sucesso.');
+    })
+  } 
 }
